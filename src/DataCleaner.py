5341f01ceec45e84ca2f8a7a74af545d70fa01d4
WhySoColdHere <lavrfan2005@mail.ru>
2026-08-25 15:07:58 +0300

    Explored images condition

--- src/DataCleaner.py
+++ src/DataCleaner.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import os
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
+
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_columns', None)
 
 class DataCleaner:
     def __init__(self, path_to_dataset: str) -> None:
@@ -26,8 +29,26 @@ dc = DataCleaner(path)
 df = dc.get_clean_data()
 
 
-img = Image.open(df['filepath'].iloc[0])
-print(img.size)
-print(img.mode)
-print(img.format)
+def get_img_info(image_path):
+    image = image_width = image_height = image_mode = image_format = image_status = None
+
+    try:
+        image = Image.open(image_path)
+        image_width, image_height = image.size
+        image_mode = image.mode
+        image_format = image.format
+        image_status = 'ok'
+    except UnidentifiedImageError:
+        image_status = 'broken'
+    finally:
+        return image_path, image_width, image_height, image_mode, image_format, image_status
+
+
+df_without_aug = df[~df['filepath'].str.contains('_aug')]
+images_info_series = df_without_aug['filepath'].apply(get_img_info)
+images_info_df = pd.DataFrame([*images_info_series], columns=['filepath', 'image_width', 'image_height', 'image_mode', 'image_format', 'image_status'])
+df_without_aug_images_info = pd.merge(left=df_without_aug, right=images_info_df, on='filepath')
+
+print(df_without_aug_images_info.groupby('label')['image_status'].value_counts().reset_index().pivot(index='label', columns='image_status', values='count'))
+
 
