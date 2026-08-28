fb650f57b0f75625d39d677b93f2670d675fa580
WhySoColdHere <lavrfan2005@mail.ru>
2026-08-28 23:15:41 +0300

    Updated cnn class, added base trainig chain

--- src/cnn.py
+++ src/cnn.py
@@ -1,13 +1,19 @@
+import torch
 from pandas import Series
 from cosmic_dataset import CosmicDataset
 import torchvision
 from torch.utils.data import DataLoader
+import torch.nn as nn
 
-class CNN:
+
+class CosmicCNN(nn.Module):
     def __init__(self, dataset: Series):
+        super().__init__()
         self.batch_size = 32
         self.common_img_size = (128, 128)
         self.labels = dataset['labels']
+        self.epochs = 10
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         self.train_transform = None
         self.val_transform = None
@@ -20,6 +26,20 @@ class CNN:
         self.train_loader = None
         self.val_loader = None
         self.test_loader = None
+
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.relu = nn.ReLU()
+        self.flatten = nn.Flatten()
+        self.layer1 = nn.Linear(65536, 512)
+        self.layer2 = nn.Linear(512, 128)
+        self.layer_out = nn.Linear(128, 8)
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=0.001)
+
+        self.to(self.device)
 
         self._create_transforms()
         self._create_datasets(dataset)
@@ -63,7 +83,35 @@ class CNN:
             shuffle=False
         )
 
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool1(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool2(x)
+        x = self.flatten(x)
+        x = self.layer1(x)
+        x = self.relu(x)
+        x = self.layer2(x)
+        x = self.relu(x)
+        x = self.layer_out(x)
+
+        return x
+
     def see_the_world_my_child(self):
-        images, labels = next(iter(self.train_loader))
-        print(images[0].shape)
-        print(self.train_dataset[0][0].shape)
+        x_batch_img, y_batch_lb = next(iter(self.train_loader))
+
+        self.optimizer.zero_grad()
+        x_batch_img = x_batch_img.to(self.device)
+        y_batch_lb = y_batch_lb.to(self.device)
+
+        output = self(x_batch_img)
+        loss = self.criterion(output, y_batch_lb)
+
+        loss.backward()
+
+        self.optimizer.step()
+
+        print(loss)
+        print(loss.shape)
--- src/controller.py
+++ src/controller.py
@@ -1,12 +1,14 @@
 from conf import DATASET_PATH
 from data_cleaning import DataCleaner
-from cnn import CNN
+from cnn import CosmicCNN
 
 
 class Controller:
     def __init__(self):
-        cnn = CNN(DataCleaner(DATASET_PATH).get_split_data())
+        cnn = CosmicCNN(DataCleaner(DATASET_PATH).get_split_data())
         cnn.see_the_world_my_child()
 
 controller = Controller()
+
+
 
